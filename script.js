646ef8d36af347d0499f9003a6bbf81c543f4abf
Gürkan <gurkanyigitgr@gmail.com>
2023-04-06 15:40:26 +0300

    hidden posts

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const btn = document.querySelector('.btn-open');
-const form = document.querySelector('.fact-form');
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
 
-btn.addEventListener('click', function() {
-    if(form.classList.contains('hidden')){
-        form.classList.remove('hidden');
-        btn.textContent = 'Close';
-    }else{
-        form.classList.add('hidden');
-        btn.textContent = 'Share a Fact'
-    }
-})
+const factsList = document.querySelector(".facts-list");
+
+factsList.innerHTML = "";
+
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a Fact";
+  }
+});
